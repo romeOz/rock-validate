f9cd43cf3797ec06bbeac073a0e8ec4b1e1e278c
romeOz <serggalka@gmail.com>
2016-10-07 20:10:53 +0300

    Added support unicode and IP of domain to validation email.

--- src/rules/Email.php
+++ src/rules/Email.php
@@ -3,6 +3,8 @@
 namespace rock\validate\rules;
 
 
+use True\Punycode;
+
 class Email extends Rule
 {
     /**
@@ -10,9 +12,39 @@ class Email extends Rule
      */
     public function validate($input)
     {
-        return is_string($input) && preg_match(
-            '/^(\\w+[\\w\.\+\-]+)?\\w+@(\\w+\.)+\\w+$/iu',
-            $input
-        );
+        return is_string($input) && preg_match('/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E\pL\pN]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F\pL\pN]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E\pL\pN]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F\pL\pN]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iDu', $this->encodeEmail($input));
     }
-} 
+
+    /**
+     * Encode an internationalized domain name
+     *
+     * @param string $domain The domain name
+     * @return string The encoded domain name
+     */
+    protected function encode($domain)
+    {
+        $objPunycode = new Punycode();
+        return $objPunycode->encode($domain);
+    }
+
+    /**
+     * Encode all e-mail addresses within a string
+     *
+     * @param string $email The string to encode
+     * @return string The encoded string
+     */
+    protected function encodeEmail($email)
+    {
+        if ($email == '')
+        {
+            return '';
+        }
+        if (strpos($email, '@') === false)
+        {
+            return $email; // see #6241
+        }
+        $arrChunks = explode('@', $email);
+        $strHost = array_pop($arrChunks);
+        return implode('@', $arrChunks) . '@' . static::encode($strHost);
+    }
+}
--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -16,7 +16,7 @@ class EmailTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @dataProvider roviderInvalid
+     * @dataProvider providerInvalid
      */
     public function testInvalid($email)
     {
@@ -31,23 +31,53 @@ class EmailTest extends \PHPUnit_Framework_TestCase
             ['mail+mail@gmail.com'],
             ['test@тест.рф'],
             ['mail.email@e.test.com'],
-            ['a@a.a']
+            ['a@a.a'],
+            ['петя@тест.рф'],
+            ['test@золотой-ларец.рф'],
+            ['test@золотой--ларец.рф'],
+            ['test@test.xn--p1ai'],
+            ['contact@xn----8sbpfklcuba0as3d.xn--p1ai'],
+            ['test@exämple.com'],
+            ['test@ä-.xe'],
+            ['test@wähwähwäh.ümläüts.de'],
+            ['user@[255.255.255.255]'],
+            ['user@[IPv6:2001:db8:1ff::a0b:dbd0]'],
+            ['user@[IPv6:2001:0db8:85a3:08d3:1319:8a2e:0370:7344]'],
+            ['user@[IPv6:1111:2222:3333:4444:5555:6666:255.255.255.255]']
         ];
     }
 
-    public function roviderInvalid()
+    public function providerInvalid()
     {
         return [
+            ['test@t@test'],
+            [' test@t@test'],
             ['test@test'],
             ['test'],
+            ['test@'],
             ['@test.com'],
+            ['@'],
             ['mail@test@test.com'],
+            ['test@test.com-'],
+            ['test..child@example.com'],
+            ['test@sub.-example.com'],
+            ['test@_smtp_.example.com'],
+            ['test@sub.-example.com'],
+            ['(comment)test@test.com'],
+            ['this\ still\"not\allowed@example.com'],
             ['test.test@'],
             ['test.@test.com'],
             ['test@.test.com'],
             ['test@test..com'],
             ['test@test.com.'],
-            ['.test@test.com']
+            ['.test@test.com'],
+            ['test@a[255.255.255.255]'],
+            ['test@[255.255.255]'],
+            ['test@[255.255.255.255.255]'],
+            ['test@[255.255.255.256]'],
+            ['test@[2001::7344]'],
+            ['test@[IPv6:1111:2222:3333:4444:5555:6666:7777:255.255.255.255]'],
+            ['test@[1.2.3.4'],
         ];
     }
 }
